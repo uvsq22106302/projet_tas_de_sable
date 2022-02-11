4a526f28ea646f1bfdbbdf15a288853b2aff70f7
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-02-11 12:49:01 +0100

    fonction maj commencée

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -13,20 +13,28 @@ import random as rd
 H = 500
 W = 500
 
-def creation_random () :
-    Config_random= [[], [], []]
-    print(Config_random)
-    for i in range (3):
-        for j in range (3):
-            Config_random[i] += [rd.randint(0, 6)]
-    return(Config_random)
+def config_courante (p, q):
+    global config
+    config = [[0] * p for i in range(q)]
 
+def maj ():
+    global config
+    for i in range (len(config) - 1):
+        for j in range (len(config[0]) - 1):
+            if config[i][j] == 0 :
+                color = "purple"
+            if config[i][j] == 1 :
+                color = "blue"
+            if config[i][j] == 2 :
+                color = "green"
+            if config[i][j] == 3 :
+                color = "yellow"
+            
 racine=tk.Tk()
-canvas=tk.Canvas(racine, height=H, width=W, relief="ridge",borderwidth=3)
-Bouton= tk.Button(racine, padx=20,font=("lines","10"),text="...",relief="ridge",borderwidth=3)
+canvas=tk.Canvas(racine, height = H, width = W, relief="ridge",borderwidth = 3)
+Bouton= tk.Button(racine, padx = 20, font = ("lines", "10"), text = "...", relief = "ridge", borderwidth = 3)
 
-canvas.grid(column=1,row=0)
-Bouton.grid(column=0,row=0,rowspan=1)
+canvas.grid(column = 1, row = 0)
+Bouton.grid(column = 0, row = 0, rowspan = 1)
 
 racine.mainloop()
-
