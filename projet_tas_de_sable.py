b0aaa0df6d4701d8fab9dd504e4c4683cf35bbb9
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-02-11 11:21:17 +0100

    modif test 3

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -5,12 +5,12 @@
 # Bryan LE BLANC
 # Kais CHEBOUB
 # https://github.com/uvsq22106302/projet_tas_de_sable
-####################################### 
+#####################################################
+
 import tkinter as tk
 
-
 racine=tk.Tk()
-canvas=tk.Canvas(racine,height=H,widht=W)
+canvas=tk.Canvas(racine, height=H, widht=W)
 
 
 
