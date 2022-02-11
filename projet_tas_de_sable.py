abc7850ba1e1769bd7ad7ffecf42e79a1d5f9b95
uvsq22106302 <alexandre.chollet@ens.uvsq.fr>
2022-02-11 12:20:38 +0100

    modif

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -9,8 +9,15 @@
 
 import tkinter as tk
 
-H = 100
-W = 100
+H = 500
+W = 500
 
 racine=tk.Tk()
-canvas=tk.Canvas(racine, height=H, widht=W)
+canvas=tk.Canvas(racine, height=H, width=W, relief="ridge",borderwidth=3)
+Bouton= tk.Button(racine, padx=20,font=("lines","10"),text="...",relief="ridge",borderwidth=3)
+
+canvas.grid(column=1,row=0)
+Bouton.grid(column=0,row=0,rowspan=1)
+
+racine.mainloop()
+
