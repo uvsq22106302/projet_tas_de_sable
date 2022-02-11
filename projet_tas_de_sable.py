63baa89ca29613834f4b6672ec6d2444729ddfbc
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-02-11 12:21:41 +0100

    ajout fonction liste de liste random

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -8,9 +8,18 @@
 #####################################################
 
 import tkinter as tk
+import random as rd
 
 H = 100
 W = 100
+
+def creation_random () :
+    Config_random= [[], [], []]
+    print(Config_random)
+    for i in range (3):
+        for j in range (3):
+            Config_random[i] += [rd.randint(0, 6)]
+    return(Config_random)
 
 racine=tk.Tk()
 canvas=tk.Canvas(racine, height=H, widht=W)
