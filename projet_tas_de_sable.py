36408fc47ffc16a4b995272203a46b419d33e6da
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-02-24 21:59:28 +0100

    Petites modifs

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -17,7 +17,7 @@ import random as rd
 #Constantes
 
 #taille de la grille
-taille = 5
+taille = 9
 
 #delai d'affichage entre chaque changement de couleur de chaque case pendant l'iteration
 delai = 100
@@ -29,11 +29,15 @@ L_coul = ["black", "red", "green", "blue", "white", "yellow", "cyan", "magenta"]
 #Variables globales
 
 stop = 0
+etape = 0
 
 ###############################################################################################################################################
 #Fonctions
 
 def init ():
+    """
+    Initialise la grille
+    """
     global config
     global L_obj
     config = [[0] * taille for i in range(taille)]
@@ -42,13 +46,19 @@ def init ():
         for j in range (taille) :
             L_obj[i] += [canvas.create_rectangle((i * coeff, j * coeff), ((i+1) * coeff, (j+1) * coeff), fill = L_coul[0], outline = L_coul[0])]
 
+def addition ():
+    pass
+
+def soustraction ():
+    pass
+
 def config_alea ():
     """
     Créé une configuration aléatoire de taille (taille x taille) puis l'affiche grâce à la fonction maj
     """
     for i in range (taille):
         for j in range (taille):
-            config[i][j] = rd.randint(0, 3)
+            config[i][j] = rd.randint(0, 4)
             canvas.itemconfigure(L_obj[i][j], fill = L_coul[config[i][j]], outline = L_coul[config[i][j]])
 
 def config_pile ():
@@ -60,7 +70,13 @@ def config_max ():
 def config_iden ():
     pass
 
+def config_clic():
+    pass
+
 def sauvegarde ():
+    """
+    Supprime tout ce qu'il y a dans le fichier Sauvegarde.txt et sauvegarde la configuration (liste config) et la taille dans ce fichier
+    """
     file = open("sauvegarde.txt", "w")
     file.write(str(taille) + "\n")
     for i in range (taille):
@@ -69,6 +85,9 @@ def sauvegarde ():
     file.close()
 
 def config_save ():
+    """
+    Récupère la taille dans le fichier Sauvegarde.txt puis réinitialise la grille puis récupère la configuration dans Sauvegarde.txt et l'affiche sur la grille
+    """
     global taille, coeff
     file = open("sauvegarde.txt", "r")
     taille = int(file.readline())
@@ -90,7 +109,8 @@ def iteration():
     Si rien n'a été modifié pendant la dernière itération : arrêt de l'itération et modification du bouton bouton_iter
     """
     global stop
-    bouton_iter.config(text = "Stopper l'itération", command = arret)
+    if etape == 0 :
+        bouton_iter.config(text = "Stopper l'itération", command = arret)
     compteur = 0
     for i in range (taille):
         for j in range (taille):
@@ -115,17 +135,25 @@ def iteration():
                     config[i][j-1] += 1
                     canvas.itemconfigure(L_obj[i][j-1], fill = L_coul[config[i][j-1]], outline = L_coul[config[i][j-1]])
     racine.after(delai, racine.update())
-    if compteur != 0 and stop == 0:
+    if compteur != 0 and stop == 0 and etape == 0:
         racine.after(0, iteration)
     if compteur == 0:
         bouton_iter.config(text = "Lancer l'itération", command = iteration)
 
 def arret ():
     """
-    Fonction qui change la variable stop en 1
+    Change la variable stop en 1
     """
     global stop
     stop = 1
+
+def iteration_unique():
+    """
+    Change la variable etape en 1
+    """
+    global etape
+    etape = 1
+    iteration()
 
 ###############################################################################################################################################
 #Partie principale
@@ -144,6 +172,7 @@ bouton_max = tk.Button(racine, text = "Configuration Max Stable", command = conf
 bouton_iden = tk.Button(racine, text = "Configuration Identity", command = config_iden)
 bouton_save = tk.Button(racine, text = "Configuration sauvegardée", command = config_save)
 bouton_iter = tk.Button(racine, text = "Lancer l'itération", command = iteration)
+bouton_etape = tk.Button(racine, text = "Faire une étape de l'itération", command = iteration_unique)
 bouton_sauv = tk.Button(racine, text = 'Sauvegarder la configuration actuelle', command = sauvegarde)
 
 #Placement des widgets
@@ -154,7 +183,8 @@ bouton_max.pack(side = "top", fill = "x")
 bouton_iden.pack(side = "top", fill = "x")
 bouton_save.pack(side = "top", fill = "x")
 bouton_iter.pack(side = "bottom", fill = "x")
-bouton_sauv.pack(side = "bottom", fill = "x")
+bouton_etape.pack(side = "bottom", fill = "x")
+bouton_sauv.pack(side = "left", fill = "x")
 
 #Initialisation
 init()
